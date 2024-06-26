b55dace02baad3db32f2a525b6eefc749def771b
camligo <camilla.ligovic@gmail.com>
2024-06-26 22:00:57 +1000

    updates topnav with dropdown

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ displayCurrentTime();
 setInterval(() => {
   displayCurrentTime;
 }, 60000); // updates every minute
+
+
+// Menu dropdown
+const dropdownItems = document.querySelectorAll('.dropdown');
+
+// add condition that only one of the dropdowns can be opened at once
+dropdownItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const dropdownMenu = item.querySelector('.dropdown-list');
+    const isActive = dropdownMenu.classList.contains('active');
+
+    // close all menus
+    document.querySelectorAll('.dropdown-list').forEach(menu => {
+      menu.classList.remove('active');
+    });
+
+    if (!isActive) {
+      dropdownMenu.classList.add('active');
+    }
+  });
+});
